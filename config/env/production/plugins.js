f1cea7659b2157fdc5631dad7ab00e2a0c6cb0da
Tayseer777 <tayseer@vuedale.com>
2023-02-16 10:03:21 +0300

    GG

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -1,4 +1,23 @@
 module.exports = ({env}) =>({
+  email: {
+    config: {
+      provider: 'nodemailer',
+      providerOptions: {
+        host: env('SMTP_HOST', 'smtp.example.com'),
+        port: env('SMTP_PORT', 587),
+        auth: {
+          user: env('SMTP_USERNAME'),
+          pass: env('SMTP_PASSWORD'),
+        },
+        // ... any custom nodemailer options
+        secure: true
+      },
+      settings: {
+        defaultFrom: 'tayseer@vuedale.com',
+        defaultReplyTo: 'tayseer@vuedale.com',
+      },
+    },
+  },
     upload: {
         config: {
           provider: 'aws-s3',
