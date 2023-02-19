07c32019d5fc3bb050b4a5bcdb74c239343ddc46
Tayseer777 <tayseer@vuedale.com>
2023-02-19 14:42:46 +0300

    cc

--- config/env/production/middlewares.js
+++ config/env/production/middlewares.js
@@ -1,31 +1,32 @@
 module.exports = [
     'strapi::errors',
-    {
-      name: 'strapi::security',
-      config: {
-        contentSecurityPolicy: {
-          useDefaults: true,
-          directives: {
-            'connect-src': ["'self'", 'https:'],
-            'img-src': [
-              "'self'",
-              'data:',
-              'blob:',
-              'dl.airtable.com',
-              'naffasy.s3.us-east-1.amazonaws.com',
-            ],
-            'media-src': [
-              "'self'",
-              'data:',
-              'blob:',
-              'dl.airtable.com',
-              'naffasy.s3.us-east-1.amazonaws.com',
-            ],
-            upgradeInsecureRequests: null,
-          },
-        },
-      },
-    },
+    'strapi::security',
+    // {
+    //   name: 'strapi::security',
+    //   config: {
+    //     contentSecurityPolicy: {
+    //       useDefaults: true,
+    //       directives: {
+    //         'connect-src': ["'self'", 'https:'],
+    //         'img-src': [
+    //           "'self'",
+    //           'data:',
+    //           'blob:',
+    //           'dl.airtable.com',
+    //           'naffasy.s3.us-east-1.amazonaws.com',
+    //         ],
+    //         'media-src': [
+    //           "'self'",
+    //           'data:',
+    //           'blob:',
+    //           'dl.airtable.com',
+    //           'naffasy.s3.us-east-1.amazonaws.com',
+    //         ],
+    //         upgradeInsecureRequests: null,
+    //       },
+    //     },
+    //   },
+    // },
     'strapi::cors',
     'strapi::poweredBy',
     'strapi::logger',
