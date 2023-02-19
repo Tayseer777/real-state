f587fb21040b82ad09fba2f3a39311c7d1cdb610
Tayseer777 <tayseer@vuedale.com>
2023-02-19 12:19:09 +0300

    CC

--- config/plugins.js
+++ config/plugins.js
@@ -36,30 +36,30 @@ module.exports = ({env}) =>({
           },
         },
       },
-      'place': {
-        enabled: true,
-        resolve: './src/plugins/place' // path to plugin folder
-      },
-      "google-map-picker": {
-        config: {
-        apiKey: env("GOOGLE_PUBLIC_KEY"), // required
-        default_center: { lat: 54.106438, lng: 11.556940 }, // required
-        favorites_places: [
-            {
-            title: "Berlin",
-            coordinates: { lat: 52.518536, lng: 52.518536 },
-            },
-            {
-            title: "Zurich",
-            coordinates: { lat: 47.384168, lng: 8.526831 },
-            },
-            {
-            title: "Oslo",
-            coordinates: { lat: 59.911002, lng: 10.756167},
-            },
-        ],
-        },
-    },
+      // 'place': {
+      //   enabled: true,
+      //   resolve: './src/plugins/place' // path to plugin folder
+      // },
+    //   "google-map-picker": {
+    //     config: {
+    //     apiKey: env("GOOGLE_PUBLIC_KEY"), // required
+    //     default_center: { lat: 54.106438, lng: 11.556940 }, // required
+    //     favorites_places: [
+    //         {
+    //         title: "Berlin",
+    //         coordinates: { lat: 52.518536, lng: 52.518536 },
+    //         },
+    //         {
+    //         title: "Zurich",
+    //         coordinates: { lat: 47.384168, lng: 8.526831 },
+    //         },
+    //         {
+    //         title: "Oslo",
+    //         coordinates: { lat: 59.911002, lng: 10.756167},
+    //         },
+    //     ],
+    //     },
+    // },
 })
 
 // module.exports = {
